3acc2b1150ef1e56004af83af06d9cff99eeb904
brisberg <brandon.risberg@gmail.com>
2024-03-12 21:36:25 -0700

    Adds dotenv and updated OpenAI client usage for simple prompts

--- src/main.py
+++ src/main.py
@@ -1,6 +1,32 @@
-print('Enter text to be echoed back. Enter \'exit\' to quit.')
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+
+    return {'content': response.choices[0].message.content, 'usage': response.usage}
+
+print('Enter prompt to be sent to ChatGPT. Enter \'exit\' to quit.')
 while True:
-    text = input(">: ")
-    if text == 'exit':
+    prompt = input(">: ")
+
+    if prompt == 'exit':
         exit()
-    print(text)
+
+    response = get_completion(prompt)
+
+    print(response['content'])
+    print(response['usage'])
